c56e9316ee68bc319d6a2b873f5b492682b24cb1
emanuele <emattina97@gmail.com>
2021-03-15 17:43:35 +0100

    Carico invio numeri utente funzionante

--- main.js
+++ main.js
@@ -18,13 +18,15 @@ $(document).ready(function () {
     }
     console.log(arrRandom);
 
-    $('.gioca').click(function () {
+    $('#gioca').click(function () {
+        
         for(i = 0; i < 5; i++)
         {   
             $('#num' + i).text(arrRandom[i]);
         }
 
         // far partire un timer di 30 secondi
+        // allo scadere dei 30 secondi i numeri spariscono
 
         var timer = setInterval(function (){
             var cont = parseInt($('#timer').html());
@@ -37,21 +39,27 @@ $(document).ready(function () {
 
                 for(i = 0; i < 5; i++)
                 {   
-                    $('#num' + i).html('<input type="text" id="fname" name="fname" placeholder="?" style="height: 50px; width: 70px; font-size: 32px; background: #3e5e79; border-color: #fff; border-radius: 10px;">')
+                    $('#num' + i).hide();
                 }
+                $('.simon-content.input input').show();
+                $('#invia').show();
+                $('#gioca').hide();
             }
-
         }, 1000);
+
+        // Get value on button click and show alert
+        $("#invia").click(function(){
+
+            var arrUtente = [$('#val0').val(), $('#val1').val(), $('#val2').val(), $('#val3').val(), $('#val4').val()];
+
+            console.log(arrUtente);
+        });
         
 
     });
 
-    
-    // allo scadere dei 30 secondi i numeri spariscono
     // l'utente deve scrivere i 5 numeri
     // il software dice quanti e quali numeri sono stati indovinati
-
-    
 
 });
 
